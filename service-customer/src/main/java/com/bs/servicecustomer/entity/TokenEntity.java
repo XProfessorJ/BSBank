88a3b071309d38ed1125a9ce7cac9f05a3f43f16
JinyeeJane <1157924293@qq.com>
2019-07-15 17:23:54 +0800

    handle sql without try-catch

---
+++ service-customer/src/main/java/com/bs/servicecustomer/entity/TokenEntity.java
@@ -0,0 +1,10 @@
+package com.bs.servicecustomer.entity;
+
+public class TokenEntity {
+
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+}
---
+++ service-feign/src/main/java/com/bs/servicefeign/Entity/TokenEntity.java
@@ -0,0 +1,10 @@
+package com.bs.servicefeign.Entity;
+
+public class TokenEntity {
+
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+}
